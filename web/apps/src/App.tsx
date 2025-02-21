21cc4e0589e5f51c2be3a573648a9009fd8bec35
mizuki-aoki-cleanrev <aoki.m@cleanrevolution.jp>
2025-02-21 08:58:13 +0000

    Temporary save

--- web/apps/src/App.tsx
+++ web/apps/src/App.tsx
@@ -1,49 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./Header";
+import Controller from "./Controller";
+import Chart from "./Chart";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    const backend_test = () => {
-        fetch("http://localhost:5010/")
-            .then((res) => {
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    throw new Error("Network response was not ok");
-                }
-            })
-            .then((data) => {
-                console.log(data);
-            })
-            .catch((error) => {
-                console.error("Error!", error);
-            });
-    };
-    backend_test();
-
+    // const [count, setCount] = useState(0);
+    // const [jsonData, setJsonData] = useState<Array<{ id: number; message: string }> | undefined>();
     return (
-        <>
-            <div className="w-full h-[10px] bg-red-500"></div>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        <div>
+            <Header />
+            <Controller />
+            <Chart />
+        </div>
     );
 }
 
---
+++ web/apps/src/Controller.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Controller() {
+    return <div className="h-[80px]">Controller</div>;
+}
+
+export default Controller;
