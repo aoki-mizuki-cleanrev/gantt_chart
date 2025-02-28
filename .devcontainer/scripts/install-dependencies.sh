5ce4d38bb5fafdf4413b10965d9df7cb3a9c9d08
mizuki-aoki-cleanrev <aoki.m@cleanrevolution.jp>
2025-02-28 09:10:44 +0000

    Update: add the command at container-startup

--- .devcontainer/scripts/install-dependencies.sh
+++ .devcontainer/scripts/install-dependencies.sh
@@ -3,6 +3,6 @@ set -eu -o pipefail
 
 # --- Install mysql-client
 sudo apt update
-sudo apt install -y default-mysql-client
+sudo apt install -y default-mysql-client vim
 
 echo "mysql-client installed successfully"
