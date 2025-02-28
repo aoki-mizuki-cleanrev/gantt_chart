c40df974b35a79e68ac32eabe953c39da73a591e
mizuki-aoki-cleanrev <aoki.m@cleanrevolution.jp>
2025-02-28 01:24:31 +0000

    Delete: node_modules

--- web/apps/src/components/Controller.tsx
+++ web/apps/src/components/Controller.tsx
@@ -4,7 +4,8 @@ import Button from "./common/Button";
 function Controller() {
     return (
         <div className="h-[80px]">
-            <Button></Button>
+            <Button disabled>aaaass</Button>
+            <Button>aaaass</Button>
         </div>
     );
 }
