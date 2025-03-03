ca74fbae03c4f0ce92cd5b5c92ee5ce833a6692b
mizuki-aoki-cleanrev <aoki.m@cleanrevolution.jp>
2025-03-03 02:02:19 +0000

    TEST: eslint

--- web/apps/src/components/Header.tsx
+++ web/apps/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header() {
-    return <header className="w-full h-[50px] flex items-center px-6 bg-gray-300">header!!!!</header>;
+    return <header className="w-full h-[50px] flex items-center px-6 bg-gray-300">header???</header>;
 }
 
 export default Header;
