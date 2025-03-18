fd0724b4492b92359495a1712013435d4c94ae7e
mizuki-aoki-cleanrev <aoki.m@cleanrevolution.jp>
2025-03-18 07:23:29 +0000

    Doc: add rsuite and swr in package.json, update tig in scriptfile

--- .devcontainer/scripts/install-dependencies.sh
+++ .devcontainer/scripts/install-dependencies.sh
@@ -3,6 +3,6 @@ set -eu -o pipefail
 
 # --- Install mysql-client
 sudo apt update
-sudo apt install -y default-mysql-client vim
+sudo apt install -y default-mysql-client vim tig
 
 echo "mysql-client installed successfully"
